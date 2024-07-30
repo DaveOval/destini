c9341e91b47d3c6036c914552268d47c25f64aac
David Vazquez <dave_u@outlook.com>
2024-07-29 22:49:45 -0600

    nuevas partes del grupo

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,16 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        storyLabel.text = String(stories[0].title)
+        choice1Button.setTitle(stories[0].choice1, for: .normal)
+        choice2Button.setTitle(stories[0].choice2, for: .normal)
+        
     }
-
-
+    
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let buttonTile = sender.title(for: .normal)
+        if ( buttonTile == stories[0].choice1) {
+            storyLabel.text = String(stories[1].title)
+            choice1Button.setTitle(stories[1].choice1, for: .normal)
+            choice2Button.setTitle(stories[1].choice2, for: .normal)
+        }
+        if ( buttonTile == stories[0].choice2) {
+            storyLabel.text = String(stories[2].title)
+            choice1Button.setTitle(stories[2].choice1, for: .normal)
+            choice2Button.setTitle(stories[2].choice2, for: .normal)
+        }
+    }
+    
 }
-
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+// like type in Typescript
+struct Story {
+    let title: String
+    let choice1: String
+    let choice2: String
+}
+
+let story1 = Story(
+    title: "You see a fork in the road",
+    choice1: "Take a left",
+    choice2: "Take a right"
+)
+let story2 = Story(
+    title: "You see a tiger",
+    choice1: "Shout for help",
+    choice2: "Play dead"
+)
+let story3 = Story(
+    title: "You find a treasure chest",
+    choice1: "Open it",
+    choice2: "Check for traps"
+)
+
+let stories = [ story1 , story2, story3 ]
